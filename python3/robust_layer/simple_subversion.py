bce139faff2ae0805ccdc59d05f85c81ad802e12
fpemud <fpemud@sina.com>
2021-03-23 23:50:09 +0800

    fix

---
+++ python3/robust_layer/simple_subversion.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+# simple_subversion.py - robust simple subversion operations
+#
+# Copyright (c) 2019-2020 Fpemud <fpemud@sina.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+import os
+import time
+import subprocess
+from ._util import Util
+
+
+def clean(dest_dir):
+    assert False
+
+
+def checkout(dest_directory, url, quiet=False):
+    if quiet:
+        # FIXME
+        quietArg = ""
+    else:
+        quietArg = ""
+
+    while True:
+        try:
+            cmd = "/usr/bin/svn update %s \"%s\" \"%s\"" % (quietArg, url, dest_directory)
+            Util.shellExecWithStuckCheck(cmd, {}, quiet)
+            break
+        except Util.ProcessStuckError:
+            time.sleep(Util.RETRY_TIMEOUT)
+        except subprocess.CalledProcessError as e:
+            if e.returncode > 128:
+                # terminated by signal, no retry needed
+                raise
+            time.sleep(Util.RETRY_TIMEOUT)
+
+
+def update(dest_directory, recheckout_on_failure=False, url=None, quiet=False):
+    if recheckout_on_failure:
+        assert url is not None
+    else:
+        assert url is None
+
+    if quiet:
+        # FIXME
+        quietArg = ""
+    else:
+        quietArg = ""
+
+    mode = "update"
+    while recheckout_on_failure:
+        if not os.path.exists(dest_directory):
+            mode = "checkout"
+            break
+        if not os.path.isdir(os.path.join(dest_directory, ".svn")):
+            mode = "checkout"
+            break
+        break
+
+    while True:
+        if mode == "update":
+            clean(dest_directory)
+            try:
+                cmd = "/usr/bin/svn -C \"%s\" update %s" % (dest_directory, quietArg)
+                Util.shellExecWithStuckCheck(cmd, {}, quiet)
+                break
+            except Util.ProcessStuckError:
+                time.sleep(1.0)
+            except subprocess.CalledProcessError as e:
+                if e.returncode > 128:
+                    raise                    # terminated by signal, no retry needed
+                time.sleep(1.0)
+        elif mode == "checkout":
+            Util.forceDelete(dest_directory)
+            try:
+                cmd = "/usr/bin/svn checkout %s \"%s\" \"%s\"" % (quietArg, url, dest_directory)
+                Util.shellExecWithStuckCheck(cmd, {}, quiet)
+                break
+            except subprocess.CalledProcessError as e:
+                if e.returncode > 128:
+                    raise                    # terminated by signal, no retry needed
+                time.sleep(1.0)
+        else:
+            assert False
